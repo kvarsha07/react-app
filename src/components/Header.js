d4c8384b5f836a599621506ca53369c9bda0f11e
Pooja Karma <pooja.karma95@gmail.com>
2024-02-06 23:32:48 +0530

    added img url

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <img/>
+      <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="logo" />
     </div>
   )
 }
